3ad793dfbd1ee2f1338995018de7eb3fbcec3d10
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-16 21:15:17 +0100

    basic grid layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from './components/Form';
 import Recipes from './components/Recipes';
 require('dotenv').config();
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -5,21 +5,27 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const Recipes = props => {
   const style = {
     card: {
-      maxWidth: '345px'
+      width: '100%',
+      height: '25rem'
     },
     media: {
       paddingTop: '56.25%' // 16:9
+    },
+    container: {
+      width: '90%',
+      margin: '0 auto'
     }
   };
 
   const recipeList = () =>
     props.recipes.map(recipe => {
       return (
-        <div key={recipe.recipe_id}>
+        <Grid item xs={12} sm={6} lg={3}>
           <Card style={style.card}>
             <CardMedia
               image={recipe.image_url}
@@ -27,17 +33,23 @@ const Recipes = props => {
               style={style.media}
             />
             <CardContent>
-              <Typography gutterBottom variant="headline" component="h2">
+              <Typography gutterBottom variant="title">
                 {recipe.title}
               </Typography>
             </CardContent>
             <CardActions />
           </Card>
-        </div>
+        </Grid>
       );
     });
 
-  return <div>{recipeList()}</div>;
+  return (
+    <div style={style.container}>
+      <Grid container spacing={16} alignItems="center">
+        {recipeList()}
+      </Grid>
+    </div>
+  );
 };
 
 export default Recipes;
