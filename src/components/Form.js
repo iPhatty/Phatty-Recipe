39cec4ab714035f9ff97b16284d2db562d84f02b
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-16 19:56:40 +0100

    material UI form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
 const Form = props => {
+  const style = {
+    button: {
+      marginLeft: '1rem'
+    },
+    form: {
+      margin: '2rem 0'
+    }
+  };
   return (
-    <form onSubmit={props.getRecipe}>
-      <input type="text" name="recipeName" />
-      <button>Im hungry!</button>
+    <form onSubmit={props.getRecipe} style={style.form}>
+      <Input
+        placeholder="Search recipes here..."
+        type="text"
+        name="recipeName"
+        inputProps={{
+          'aria-label': 'Recipe Search'
+        }}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="secondary"
+        style={style.button}
+      >
+        Im hungry!
+      </Button>
     </form>
   );
 };
