e130af6bf050a77c83b9c3693ed2944a1e263bb6
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-17 01:10:57 +0100

    readjust postion of loader

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -20,16 +20,19 @@ const style = {
   },
   container: {
     width: '90%',
-    margin: '0 auto'
+    height: '100vh',
+    margin: '0 auto',
+    position: 'relative'
   },
   link: {
     color: '#e53935'
   },
   spinner: {
-    width: '100px',
+    margin: '0',
     position: 'absolute',
     top: '50%',
-    left: '50%'
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
   }
 };
 class Recipe extends React.Component {
